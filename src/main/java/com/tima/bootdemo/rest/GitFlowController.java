2df789714166c9e4e94056df9cfc631ddebab345
shuli.li <shuli.li@timanetworks.com>
2019-08-08 14:39:59 +0800

    first commit

---
+++ src/main/java/com/tima/bootdemo/rest/GitFlowController.java
@@ -0,0 +1,14 @@
+package com.tima.bootdemo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitFlowController {
+
+    @GetMapping("/hello")
+    public String getString(){
+        System.out.println("feature test 0.0.1-SNAPSHOT");
+        return "hello gitflow";
+    }
+}
